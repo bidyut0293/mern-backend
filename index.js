da397fe4e252d883c635b045c45bf0e3abea1e1b
bidyut0293 <23bidyut45@gmail.com>
2024-05-21 20:00:22 +0530

    API Connection

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const cors = request("cors");
+const cors = require('cors')
+require('./db/config');
+const User = require("./db/user");
 const app = express();
 
-app.get("/", (req, resp) => {
-    resp.send("testing")
-});
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async(req, resp)=>{
+    let user= new User(req.body);
+    let result = await user.save();
+    resp.send(result)
+})
 
 app.listen(5000)
